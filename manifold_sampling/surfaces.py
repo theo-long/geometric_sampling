da26a9e71bded5b399fa84f9eac56b2935d2f6d9
Theo Long <theodorelong98@gmail.com>
2023-02-20 17:59:29 +0000

    black formatting

--- manifold_sampling/surfaces.py
+++ manifold_sampling/surfaces.py
@@ -2,26 +2,32 @@ from geometric_sampling.manifold_sampling.errors import ConstraintError
 from typing import Callable, Optional
 import torch
 
+
 class ConstraintSurface:
-    
-    def __init__(self, n_dim:int, constraint_equation: Callable[[torch.Tensor], torch.Tensor], metric: Optional[Callable]=None, tol=1e-6) -> None:
+    def __init__(
+        self,
+        n_dim: int,
+        constraint_equation: Callable[[torch.Tensor], torch.Tensor],
+        metric: Optional[Callable] = None,
+        tol=1e-6,
+    ) -> None:
         self.n_dim = n_dim
         self.constraint_equation = constraint_equation
         if metric:
             self.metric = metric
         else:
-            self.metric =  _euclidean_metric
+            self.metric = _euclidean_metric
         self.tol = tol
 
     def generate_tangent_space(self, x: torch.Tensor):
-        '''Generates the tangent space at a point x, given as an orthnormal basis.'''
+        """Generates the tangent space at a point x, given as an orthnormal basis."""
         normal = self.generate_normal_space(x)
-        Q, R = torch.linalg.qr(normal, mode='complete')
-        Q = Q[:,-self.n_dim+1:]
+        Q, R = torch.linalg.qr(normal, mode="complete")
+        Q = Q[:, -self.n_dim + 1 :]
         return Q
 
     def generate_normal_space(self, x: torch.Tensor):
-        '''Generates the subspace normal to the tangent space at x, given as an orthonormal basis.'''
+        """Generates the subspace normal to the tangent space at x, given as an orthonormal basis."""
         x = x.requires_grad_()
         constraint_value = self._check_constraint(x)
         constraint_value.backward()
@@ -32,9 +38,10 @@ class ConstraintSurface:
     def _check_constraint(self, x):
         constraint_value = self.constraint_equation(x)
         if constraint_value > self.tol:
-            raise ConstraintError(f"Point x does not satisfy constraint equation with tolerance {self.tol:.2E}")
+            raise ConstraintError(
+                f"Point x does not satisfy constraint equation with tolerance {self.tol:.2E}"
+            )
         return constraint_value
-
 
 
 def _euclidean_metric(x):
