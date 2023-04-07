fb11918bd570a7572e3ea3af4689f22d252ebf63
Theo Long <theodorelong98@gmail.com>
2023-04-07 16:39:42 -0400

    add ability to specify distribution

--- manifold_sampling/plotting.py
+++ manifold_sampling/plotting.py
@@ -1,19 +1,24 @@
 import numpy as np
 import plotly.graph_objects as go
 
-def plot_torus(r, R, precision=50):
+def plot_torus(r, R, p=None, precision=50):
     U = np.linspace(0, 2*np.pi, precision)
     V = np.linspace(0, 2*np.pi, precision)
     U, V = np.meshgrid(U, V)
     X = (R+r*np.cos(V))*np.cos(U)
     Y = (R+r*np.cos(V))*np.sin(U)
     Z = r*np.sin(V)
+
+    if p is None:
+        color = None
+    else:
+        color = p(np.stack([X, Y, Z]))
     
-    fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y, colorscale="viridis")])
+    fig = go.Figure(data=[go.Surface(z=Z, x=X, y=Y, colorscale="viridis", surfacecolor=color)])
     fig.update_layout(autosize=True)
     return fig
 
-def plot_sphere(r, precision=50):
+def plot_sphere(r, p=None, precision=50):
     U = np.linspace(0, 2*np.pi, precision)
     V = np.linspace(0, np.pi, precision)
     U, V = np.meshgrid(U, V)
@@ -21,6 +26,28 @@ def plot_sphere(r, precision=50):
     y = r * np.sin(U)*np.sin(V)
     z = r * np.cos(V)
 
-    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y, colorscale="viridis")])
+    if p is None:
+        color = None
+    else:
+        color = p(np.stack([x, y, z]))
+
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y, colorscale="viridis", surfacecolor=color)])
+    fig.update_layout(autosize=True)
+    return fig
+
+def plot_ellipsoid(x_factor, y_factor, z_factor, p=None, precision=50):
+    U = np.linspace(0, 2*np.pi, precision)
+    V = np.linspace(0, np.pi, precision)
+    U, V = np.meshgrid(U, V)
+    x = np.cos(U)*np.sin(V) / np.sqrt(x_factor)
+    y = np.sin(U)*np.sin(V) / np.sqrt(y_factor)
+    z = np.cos(V) / np.sqrt(z_factor)
+
+    if p is None:
+        color = None
+    else:
+        color = p(np.stack([x, y, z]))
+
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y, colorscale="viridis", surfacecolor=color)])
     fig.update_layout(autosize=True)
     return fig
