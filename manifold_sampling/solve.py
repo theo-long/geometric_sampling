5f6326ea4e63700c899a13cdb2c6151cee72ea63
Theo Long <theodorelong98@gmail.com>
2023-05-06 14:53:30 +0100

    formatting

--- manifold_sampling/solve.py
+++ manifold_sampling/solve.py
@@ -1,20 +1,22 @@
 from geometric_sampling.manifold_sampling.surfaces import AlgebraicSurface
-from geometric_sampling.manifold_sampling.utils import sympy_func_to_array_func, jitted_norm
+from geometric_sampling.manifold_sampling.utils import (
+    sympy_func_to_array_func,
+    jitted_norm,
+)
 
 import sympy
 import numpy as np
 from numba import njit, float64
 from scipy import optimize
 
-t = sympy.symbols('t')
+t = sympy.symbols("t")
 NEWTON_MAX_ITER = 50
+
 
 def generate_line_equation_coefficients(surface: AlgebraicSurface):
     """Symbolically generate the coefficients of the polynomial corresponding to the restriction
     of the surface equation to the line p-->q."""
-    p, q = sympy.symbols(f"p:{surface.n_dim}"), sympy.symbols(
-        f"q:{surface.n_dim}"
-    )
+    p, q = sympy.symbols(f"p:{surface.n_dim}"), sympy.symbols(f"q:{surface.n_dim}")
     line = sympy.Matrix(p) + t * (sympy.Matrix(q) - sympy.Matrix(p))
     exp = surface.algebraic_equation[0].subs(
         zip(sympy.symbols(f"x:{surface.n_dim}"), line)
@@ -23,11 +25,12 @@ def generate_line_equation_coefficients(surface: AlgebraicSurface):
     line_eq_coeffs = sympy_func_to_array_func(p + q, coeffs)
     return line_eq_coeffs
 
+
 def find_line_intersections(
     line_eq_coeffs: np.ndarray,
     p1: np.ndarray,
     p2: np.ndarray,
-    return_all: bool=False,
+    return_all: bool = False,
 ):
     """Find solution to constraint on line p1-->p2, or return None if not found.
 
@@ -39,7 +42,9 @@ def find_line_intersections(
     roots = np.roots(line_eq_coeffs)
 
     # Only accept roots in sphere
-    sols_mask = (np.abs(np.imag(roots)) < 1e-14) & (np.real(roots) >= 0) & (np.real(roots) <= 1)
+    sols_mask = (
+        (np.abs(np.imag(roots)) < 1e-14) & (np.real(roots) >= 0) & (np.real(roots) <= 1)
+    )
     sols = np.real(roots[sols_mask])
 
     if len(sols) == 0:
@@ -49,12 +54,13 @@ def find_line_intersections(
     else:
         return p1 + np.random.choice(sols) * (p2 - p1)
 
+
 @njit(float64[:](float64[:], float64[:, :], float64[:]))
 def _newton_inner_loop(x, j_x, f_val):
     delta = np.linalg.solve(j_x, -f_val)
     x += delta
     return x
-    
+
 
 def newton_solver(F, J, x, eps):
     """
@@ -77,6 +83,7 @@ def newton_solver(F, J, x, eps):
     if F_norm > eps:
         x = None
     return x, iteration_counter
+
 
 def scipy_solver(F, J, x, eps, method="hybr"):
     result = optimize.root(
